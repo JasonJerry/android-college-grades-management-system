381b702f3629d7d62c34c793f7acd3b0360e9dc9
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2017-03-26 17:28:12 +0530

    Implement Edit Student Details

--- app/src/main/java/com/example/menuka/loginandregistration/StudentDetailsForm.java
+++ app/src/main/java/com/example/menuka/loginandregistration/StudentDetailsForm.java
@@ -10,7 +10,10 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import firebase.Connection;
 import models.Student;
@@ -23,25 +26,37 @@ public class StudentDetailsForm extends AppCompatActivity {
     private EditText birthdayEditText;
     private Button btnSave;
     private DatabaseReference databaseReference;
+    private ArrayAdapter<CharSequence> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_details_form);
 
-        databaseReference = Connection.getINSTANCE().getDatabaseReference().child("students");
-
-        departmentsSpinner = (Spinner) findViewById(R.id.departmentsSpinner);
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.deparments_array, android.R.layout.simple_spinner_dropdown_item);
-        departmentsSpinner.setAdapter(arrayAdapter);
-
-        firstNameEditText = (EditText) findViewById(R.id.firstNameEditText);
-        lastNameEditText = (EditText) findViewById(R.id.lastNameEditText);
-        indexNoEditText = (EditText) findViewById(R.id.indexNoEditText);
-        birthdayEditText = (EditText) findViewById(R.id.birthdayEditText);
-        btnSave = (Button) findViewById(R.id.btnSave);
+        initComponents();
 
         final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null) {
+            databaseReference.child(firebaseAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Student s = dataSnapshot.getValue(Student.class);
+                    firstNameEditText.setText(s.getFirstName());
+                    lastNameEditText.setText(s.getLastName());
+                    birthdayEditText.setText(s.getBirthday());
+                    indexNoEditText.setText(s.getIndexNo());
+                    departmentsSpinner.setSelection(arrayAdapter.getPosition(s.getDepartment()));
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,10 +75,26 @@ public class StudentDetailsForm extends AppCompatActivity {
                     student.setUserId(uid);
 
                     databaseReference.child(student.getUserId()).setValue(student);
+
+                    StudentDetailsForm.this.finish();
                 }else{
                     Log.i("Error saving Student: ", "No user logged in.");
                 }
             }
         });
+    }
+
+    private void initComponents() {
+        databaseReference = Connection.getINSTANCE().getDatabaseReference().child("students");
+
+        departmentsSpinner = (Spinner) findViewById(R.id.departmentsSpinner);
+        arrayAdapter = ArrayAdapter.createFromResource(this, R.array.deparments_array, android.R.layout.simple_spinner_dropdown_item);
+        departmentsSpinner.setAdapter(arrayAdapter);
+
+        firstNameEditText = (EditText) findViewById(R.id.firstNameEditText);
+        lastNameEditText = (EditText) findViewById(R.id.lastNameEditText);
+        indexNoEditText = (EditText) findViewById(R.id.indexNoEditText);
+        birthdayEditText = (EditText) findViewById(R.id.birthdayEditText);
+        btnSave = (Button) findViewById(R.id.btnSave);
     }
 }
